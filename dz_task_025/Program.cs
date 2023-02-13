12089cd1522c5b88b002df03b436dd629a8d6fc8
Anton Kargapolov <igloooooook@gmail.com>
2023-02-13 20:51:23 +0500

    dz task 025

---
+++ dz_task_025/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int InputNumber(string str)
+{
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int a = InputNumber("Введите число А ");
+int b = InputNumber("Введите число B ");
+
+System.Console.WriteLine($"{a} в степени {b} равно {Math.Pow(a,b)}");

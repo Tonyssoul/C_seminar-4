e661677def30fb26b812adef35335f0fd75cbc48
Anton Kargapolov <igloooooook@gmail.com>
2023-02-13 22:42:06 +0500

    dz task 027

---
+++ dz_task_027/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int InputNumber(string str)
+{
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = InputNumber("Введите число ");
+int sum = 0;
+
+void Result(int b)
+{
+    while (b > 0)
+    {
+        sum += b % 10;
+        b /= 10;
+    }
+}
+
+Result(a);
+
+System.Console.WriteLine(sum);

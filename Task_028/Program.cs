af7f104cfce668629eec91cd03fa502358588674
Anton Kargapolov <igloooooook@gmail.com>
2023-02-13 17:39:49 +0500

    task 028

---
+++ Task_028/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+
+int InputNumber(string str)
+{
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int num = InputNumber("Введите число А ");
+
+
+int GetSumElement(int A)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result *= i; // result = result + i
+    }  
+    return result;
+}
+
+
+System.Console.WriteLine($"сумма чисел от 1 до числа {num} = {GetSumElement(num)}");

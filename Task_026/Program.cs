514e2796fd533c27f87b5bbf8dfbc98292261a8e
Anton Kargapolov <igloooooook@gmail.com>
2023-02-13 17:34:35 +0500

    task 026

---
+++ Task_026/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+
+string InputNumber(string str)
+{
+    System.Console.WriteLine(str);
+    return Console.ReadLine();
+}
+
+string a = InputNumber("Введите число ");
+int result = a.Length;
+
+System.Console.WriteLine(result);
